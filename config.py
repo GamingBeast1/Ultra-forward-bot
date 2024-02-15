da93372eb0d8884106dfffb9eeb5347b97b43663
GamingBeast1 <116881154+GamingBeast1@users.noreply.github.com>
2024-02-15 22:15:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from os import environ  
   
- class Config: 
+class Config:
      API_ID = int(environ.get("API_ID", )) 
      API_HASH = environ.get("API_HASH", "") 
      BOT_TOKEN = environ.get("BOT_TOKEN", "")  
